2334a444333f7a43f0ca1aaa18b5c345c55164dd
Ben Muschol <ben.muschol@airbnb.com>
2020-08-04 10:32:56 -0700

    pointer-events

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(function(request) {
     if(request.action === 'toggle') {
       var elem = document.getElementById('dark-mode-overlay')
       if (elem.style.cssText === '') {
-        elem.style.cssText = 'z-index:100000000000000000;width: 100vw;height: 100vh;position: fixed;top: 0;left: 0;background: white;mix-blend-mode: difference;'
+        elem.style.cssText = 'z-index:100000000000000000;pointer-events: none;width: 100vw;height: 100vh;position: fixed;top: 0;left: 0;background: white;mix-blend-mode: difference;'
       } else {
         elem.style.cssText = ''
       }
