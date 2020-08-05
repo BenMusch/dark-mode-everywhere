47df862cdd2f6f7e52fb7078ca0818211919617d
Ben Muschol <ben.muschol@airbnb.com>
2020-08-05 11:11:10 -0700

    wip

--- content.js
+++ content.js
@@ -9,7 +9,13 @@ chrome.runtime.onMessage.addListener(function(request) {
     }
 });
 
-console.log('executing!')
+/*
+var darkModeBg = document.createElement('div');
+darkModeBg.style.cssText = 'z-index:-100000000000000000;pointer-events: none;width: 100vw;height: 100vh;position: fixed;top: 0;left: 0;background: white;'
+darkModeBg.id = 'dark-mode-bg'
+document.body.appendChild(darkModeBg);
+*/
+
 var darkModeElem = document.createElement('div');
 darkModeElem.style.cssText = ''
 darkModeElem.id = 'dark-mode-overlay'
